3a2b0e300c3c01af704b7017a8cf74971e4d85c8
Vincent Le Chenadec <vincentlechenadec@gmail.com>
2026-06-07 21:14:00 +0200

    added API: no docstrings, no methods

--- src/AlgebraCore.jl
+++ src/AlgebraCore.jl
@@ -1,5 +1,19 @@
 module AlgebraCore
 
-# Write your package code here.
+export simplify,
+       substitute,
+       materialize,
+       pushforward,
+       differentiate
+
+function simplify end
+
+function substitute end
+
+function materialize end
+
+function pushforward end
+
+function differentiate end
 
 end
